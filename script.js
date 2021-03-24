c799c8438d2999bbab9bb46fd834bac1147baaa7
Mateusz Klosek <klosekmateusz@gmail.com>
2021-03-24 19:06:01 +0100

    expneses site

--- script.js
+++ script.js
@@ -9,22 +9,4 @@ function showPwd(id, el) {
   }
 } 
 
-	$(document).ready(function() {
-	var NavY = $('.nav').offset().top;
-	 
-	var stickyNav = function(){
-	var ScrollY = $(window).scrollTop();
-		  
-	if (ScrollY > NavY) { 
-		$('.nav').addClass('sticky');
-	} else {
-		$('.nav').removeClass('sticky'); 
-	}
-	};
-	 
-	stickyNav();
-	 
-	$(window).scroll(function() {
-		stickyNav();
-	});
-	});
+
